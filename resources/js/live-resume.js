b747488137a6318b9a18165d51a6ac2ec5ffc818
johyewon <ixxwxfjlwf@gmail.com>
2021-06-23 16:43:41 +0900

    js

--- resources/js/live-resume.js
+++ resources/js/live-resume.js
@@ -1,6 +1,8 @@
 $('[data-toggle="collapsible-nav"]').on('click', function(e){
     var target = ($(this).data('target'));
     $('#' + target).toggleClass('show');
+
+    $('#aside').load('include/aside.html');
 });
 
 $(document).ready(function(){
@@ -64,8 +66,17 @@ let navLinks = document.querySelectorAll(".gnb li a"); //메뉴 링크
 //window 객체에 scroll & mouse 이벤트를 추가하고 cursor 함수 실행되도록 함
 window.addEventListener("scroll", cursor);
 window.addEventListener("mousemove", cursor);
+
 //커스텀 커서의 left 값과 top 값을 커서의 XY 좌표값과 일치시킴
 function cursor(e) {
     mouseCursor.style.left = e.pageX + "px";
     mouseCursor.style.top = e.pageY - scrollY + "px";
+}
+
+var browserInfo = navigator.userAgent;    // browserInfo 에 user agent 정보를 담습니다.
+console.log(browserInfo);                                 // user agent 정보내용을 화면에 경고창으로 보여줍니다.
+if (browserInfo.indexOf("HANIX_ANDROID") > -1) {
+    console.log('android');
+    document.getElementById('header').style.visibility = 'hidden';
+    document.getElementById('header').style.display = 'none';
 }
