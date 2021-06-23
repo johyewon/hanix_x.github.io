884bc505ca3e243a59c7e0961249c0a1940a65cd
johyewon <ixxwxfjlwf@gmail.com>
2021-06-23 13:54:15 +0900

    cursor

--- resources/js/live-resume.js
+++ resources/js/live-resume.js
@@ -58,3 +58,14 @@ $('.hover-box').hover(
 
     }
 );
+
+let mouseCursor = document.querySelector(".cursor");
+let navLinks = document.querySelectorAll(".gnb li a"); //메뉴 링크
+//window 객체에 scroll & mouse 이벤트를 추가하고 cursor 함수 실행되도록 함
+window.addEventListener("scroll", cursor);
+window.addEventListener("mousemove", cursor);
+//커스텀 커서의 left 값과 top 값을 커서의 XY 좌표값과 일치시킴
+function cursor(e) {
+    mouseCursor.style.left = e.pageX + "px";
+    mouseCursor.style.top = e.pageY - scrollY + "px";
+}
